0a9985a189a9a2567f4438e03a9d6b06e11aa1d8
Adrianojunior13 <a98054099@gmail.com>
2025-08-22 12:57:27 -0300

    atualiza cores do cabeçalho e ajusta botão de busca; adiciona configuração do servidor no Vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+   host: '0.0.0.0',  // Isso faz o Vite aceitar conexões de qualquer IP
+    port: 5173,       // Se preferir, pode especificar a porta, mas o padrão é 5173
+    strictPort: true,
+}
 })
 
 
