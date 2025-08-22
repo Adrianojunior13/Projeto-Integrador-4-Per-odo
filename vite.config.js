14f88805c8aec7678e2cca5eb353436b3d2eae37
Adrianojunior13 <a98054099@gmail.com>
2025-08-22 13:16:56 -0300

    ajusta configuração do servidor no Vite para permitir conexões externas e remove comentários desnecessários

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
-    host: '0.0.0.0',  // Permite conexões externas, mas deve aceitar também localhost
-    port: 5173,       // A porta padrão
-    strictPort: true, // Garante que a porta será usada
+ server: {
+    host: '0.0.0.0',  // Isso permite que o Vite aceite conexões externas
+    port: 5173,
+    strictPort: true,
     allowedHosts: [
-      'localhost',        // Permitir localhost
-      '192.168.0.113',    // Permitir IP específico
-      'adrianodev.com.br' // Caso você precise usar o seu domínio
-    ]
+      'adrianodev.com.br',  // Adicionando o seu domínio aqui
+    ],
 }
 })
 
