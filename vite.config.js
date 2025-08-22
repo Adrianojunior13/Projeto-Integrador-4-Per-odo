fd6a81efbc34a29da9dce1a40490aad38ac25dcb
Adrianojunior13 <a98054099@gmail.com>
2025-08-22 13:03:43 -0300

    atualiza título da página para "Oportuniza - Vagas" e ajusta configuração do servidor no Vite

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-   host: '0.0.0.0',  // Isso faz o Vite aceitar conexões de qualquer IP
-    port: 5173,       // Se preferir, pode especificar a porta, mas o padrão é 5173
-    strictPort: true,
+   server: {
+    host: '0.0.0.0',  // Permite conexões externas, mas deve aceitar também localhost
+    port: 5173,       // A porta padrão
+    strictPort: true, // Garante que a porta será usada
+    allowedHosts: [
+      'localhost',        // Permitir localhost
+      '192.168.0.113',    // Permitir IP específico
+      'adrianodev.com.br' // Caso você precise usar o seu domínio
+    ]
 }
 })
 
