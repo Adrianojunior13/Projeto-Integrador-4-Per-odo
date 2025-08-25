9f3571fb2fefb21144e5935c10c9a972a8d47a76
Adrianojunior13 <a98054099@gmail.com>
2025-08-25 09:37:47 -0300

    refatoração: organiza as rotas no componente App e ajusta a importação do BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import Header from "./components/Header";
 import JobSearch from "./components/JobSearch";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
-import OportunizaPage from './pages/OportunizaPage'; // Adicionei a importação da página "OportunizaPage"
-import VagasPage from './pages/VagasPage'; // Crie este componente para Vagas
-import ContatoPage from './pages/ContatoPage'; // Crie este componente para Contato
+import OportunizaPage from './pages/OportunizaPage'; // Página "OportunizaPage"
+import VagasPage from './pages/VagasPage'; // Página "VagasPage"
+import ContatoPage from './pages/ContatoPage'; // Página "ContatoPage"
 
 function App() {
   return (
-    <Router>
+    <Router> {/* O Router deve envolver toda a aplicação, incluindo as rotas */}
       <div className="min-h-screen flex flex-col bg-gray-50">
         {/* Cabeçalho */}
         <Header />
+
+        {/* Definindo as rotas */}
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Página inicial */}
+          <Route path="/oportuniza" element={<OportunizaPage />} /> {/* Página A OPORTUNIZA */}
+          <Route path="/vagas" element={<VagasPage />} /> {/* Página VAGAS DE EMPREGO */}
+          <Route path="/contato" element={<ContatoPage />} /> {/* Página CONTATO */}
+        </Routes>
 
         {/* Conteúdo principal */}
         <main className="flex-1 max-w-6xl mx-auto p-4">
@@ -30,14 +38,6 @@ function App() {
           </div>
         </main>
       </div>
-
-      {/* Definindo as rotas */}
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* Página inicial */}
-        <Route path="/oportuniza" element={<OportunizaPage />} /> {/* Página A OPORTUNIZA */}
-        <Route path="/vagas" element={<VagasPage />} /> {/* Página VAGAS DE EMPREGO */}
-        <Route path="/contato" element={<ContatoPage />} /> {/* Página CONTATO */}
-      </Routes>
     </Router>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import OportunizaPage from './OportunizaPage'
+import OportunizaPage from "./OportunizaPage";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Router>
+  <BrowserRouter> {/* Usando BrowserRouter para envolver as rotas */}
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/oportuniza" element={<OportunizaPage />} /> {/* Nova rota */}
     </Routes>
-  </Router>
-)
+  </BrowserRouter>
+);
